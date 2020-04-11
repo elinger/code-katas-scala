76dc174f7b72e08b17b26e35c15f8c0c580cc09c
Aleksandar Stoimenov <alex.stoimenov@gmail.com>
2020-04-11 13:16:52 +0200

    Compact solution for -> roman

--- roman-numerals/src/main/scala/com/elinger/RomanNumerals.scala
+++ roman-numerals/src/main/scala/com/elinger/RomanNumerals.scala
@@ -2,99 +2,39 @@ package com.elinger
 
 object RomanNumerals {
 
+  val MapToRoman = Map(
+    1000 -> 'M',
+    500 -> 'D',
+    100 -> 'C',
+    50 -> 'L',
+    10 -> 'X',
+    5 -> 'V',
+    1 -> 'I'
+  )
+
   def toRomanNumber(dec: Int): String = {
-    require(dec > 0 && dec <= 3000, "Dec must be between 1 and 3000")
-    val (m, mr) = (dec / 1000, dec % 1000)
-    var res     = List.fill[Char](m)('M')
 
-    if (mr > 0) {
-      val (d, dr) = (mr / 500, mr % 500)
-      if (d == 1) {
-        if (dr < 400) {
-          res = res ::: List('D')
-          // calculation for C, L, X, V, I
-          res = res ::: convertValueLessThan400(dr)
-        } else {
-          res = res ::: List('C', 'M')
-          // use L, X, V, I to handle value smaller than 100
-          res = res ::: convertValueLessOrEqual100(dr - 400)
+    def helper(number: Int, base: Int): List[Char] =
+      if (number > 0) {
+        val (whole, rest) = (number / base, number % base)
+        val base10        = base / 10
+
+        whole match {
+          case w if w < 4 =>
+            List.fill[Char](w)(MapToRoman(base)) ::: helper(rest, base10)
+          case w if w >=4 && w < 5 =>
+            List(MapToRoman(base), MapToRoman(5 * base)) ::: helper(rest, base10)
+          case w if w >=5 && w < 9 =>
+            List(MapToRoman(5 * base)) ::: helper(number - 5 * base, base)
+          case _ =>
+            List(MapToRoman(base), MapToRoman(10 * base)) ::: helper(rest, base10)
         }
-      } else {
-        if (dr < 400) {
-          res = res ::: convertValueLessThan400(dr)
-        } else {
-          res = res ::: List('C', 'D')
-          res = res ::: convertValueLessOrEqual100(dr - 400)
-        }
-      }
-    }
-    res.mkString
+      } else List[Char]()
+
+    helper(dec, 1000).mkString
   }
 
-  def convertValueLessThan400(dec: Int): List[Char] = {
-    val (c, cr) = (dec / 100, dec % 100)
-    var res     = List.fill[Char](c)('C')
-    res = res ::: convertValueLessOrEqual100(cr)
-    res
-  }
 
-  def convertValueLessOrEqual100(dec: Int): List[Char] = {
-    var res = List[Char]()
-
-    if (dec < 100) {
-      val (l, lr) = (dec / 50, dec % 50)
-
-      if (l == 1) {
-        if (lr < 40) {
-          res = res ::: List('L')
-          res = res ::: convertValueLessThan40(lr)
-        } else {
-          res = res ::: List('X', 'C')
-          res = res ::: convertValueLessOrEqualTo10(lr - 40)
-        }
-      } else {
-        if (lr < 40) {
-          res = res ::: convertValueLessThan40(lr)
-        } else {
-          res = res ::: List('X', 'L')
-          res = res ::: convertValueLessOrEqualTo10(lr - 40)
-        }
-      }
-    }
-
-    res
-  }
-
-  def convertValueLessThan40(dec: Int): List[Char] = {
-    val (x, xr) = (dec / 10, dec % 10)
-    var res     = List.fill[Char](x)('X')
-    res = res ::: convertValueLessOrEqualTo10(xr)
-    res
-  }
-
-  def convertValueLessOrEqualTo10(dec: Int): List[Char] = {
-    var res = List[Char]()
-    if (dec < 10) {
-     val (v, vr) = (dec / 5, dec % 5)
-      if (v == 1) {
-        if (vr == 0) {
-          res = res ::: List('V')
-        } else if (vr < 4) {
-          res = res ::: List('V')
-          res = res ::: List.fill(vr)('I')
-        } else {
-          res = res ::: List('I', 'X')
-        }
-      } else if (vr < 4) {
-       res = res ::: List.fill[Char](vr)('I')
-      } else {
-        res = res ::: List('I', 'V')
-      }
-    }
-    res
-  }
-
-  def main(args: Array[String]): Unit = {
+  def main(args: Array[String]): Unit =
     println(toRomanNumber(1998))
-  }
 }
--- roman-numerals/src/test/scala/com.elinger/RomanNumeralsSpec.scala
+++ roman-numerals/src/test/scala/com.elinger/RomanNumeralsSpec.scala
@@ -6,10 +6,15 @@ class RomanNumeralsSpec extends FlatSpec {
 
   "The conversion" should "should work" in {
     assert(RomanNumerals.toRomanNumber(2020) === "MMXX")
+    assert(RomanNumerals.toRomanNumber(2798) === "MMDCCXCVIII")
     assert(RomanNumerals.toRomanNumber(8) === "VIII")
+    assert(RomanNumerals.toRomanNumber(5) === "V")
+    assert(RomanNumerals.toRomanNumber(3) === "III")
+    assert(RomanNumerals.toRomanNumber(9) === "IX")
     assert(RomanNumerals.toRomanNumber(31) === "XXXI")
     assert(RomanNumerals.toRomanNumber(80) === "LXXX")
     assert(RomanNumerals.toRomanNumber(369) === "CCCLXIX")
+    assert(RomanNumerals.toRomanNumber(351) === "CCCLI")
     assert(RomanNumerals.toRomanNumber(1998) === "MCMXCVIII")
   }
 }
